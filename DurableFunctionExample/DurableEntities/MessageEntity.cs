859a69d866c4f6bafc5b3b012f1392ea90bb2e26
Andrey Temchyshen <andrey.temchyshen@dataart.com>
2025-07-06 15:59:12 +0300

    refactoring done

--- DurableFunctionExample/DurableEntities/MessageEntity.cs
+++ DurableFunctionExample/DurableEntities/MessageEntity.cs
@@ -5,19 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace OrchestrationExample.DurableEntities;
-internal class MessageEntity
+internal class MessageEntity : BaseEntity
 {
-    private string _message = string.Empty;
-    public void SetMessage(string message)
-    {
-        _message = message;
-    }
-    public string GetMessage()
-    {
-        return _message;
-    }
-    public void ClearMessage()
-    {
-        _message = string.Empty;
-    }
+
 }
