1a9ed16d63a8e01ca1d1f898975276d4f448405d
Andrey Temchyshen <andrey.temchyshen@dataart.com>
2025-07-06 16:07:55 +0300

    shared log functionality added

--- DurableFunctionExample/BaseService.cs
+++ DurableFunctionExample/BaseService.cs
@@ -7,4 +7,12 @@ using System.Threading.Tasks;
 namespace OrchestrationExample;
 internal class BaseService
 {
+    // This class can be used to define common functionality for services in the orchestration example.
+    // Currently, it is empty but can be extended in the future as needed.
+    // For example, you might want to add logging, error handling, or other shared methods here.
+    void Log(string message)
+    {
+        // Placeholder for logging functionality
+        Console.WriteLine(message);
+    }
 }
